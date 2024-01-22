72617ba442af431bb92818fc678c27fc825f3e26
j1_burda <j1_burda@utb.cz>
2024-01-22 20:38:54 +0100

    znovu funkcni redirect

--- src/pages/DashboardIframe.tsx
+++ src/pages/DashboardIframe.tsx
@@ -7,7 +7,7 @@ const DashboardIframe = () => {
     <div className="iframe-container">
       <iframe
         className="responsive-iframe"
-        src="https://elfsight.com/youtube-channel-plugin-yottie/iframe/"   //https://dashboard.satnogs.org/goto/zWeN2S5Ik //https://elfsight.com/youtube-channel-plugin-yottie/iframe/
+        src="https://rss.app/embed/v1/list/trm4QTTl8rN509nE"   //https://dashboard.satnogs.org/goto/zWeN2S5Ik //https://elfsight.com/youtube-channel-plugin-yottie/iframe/
         frameBorder="0"
         allowFullScreen
       ></iframe>
